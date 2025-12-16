8d2249a397b42f6ecbd3b29db115844dd995a374
shxrox <sharondeva8@gmail.com>
2025-12-16 22:22:59 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <Routes>
    
-    <Route path="/Profile" element={<Home />} />
+    <Route index element={<Home />} /> 
     <Route path="*" element={<Error />} />
     </Routes>
  )
