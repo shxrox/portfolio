eb8abcecc85968da94bed3278188b7540bef05af
shxrox <sharondeva8@gmail.com>
2025-12-16 21:58:24 +0530

    updatde

--- vite.config.js
+++ vite.config.js
@@ -18,11 +18,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import { fileURLToPath } from 'url';
+
+// Fix for __dirname in ESM if needed
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // 👈 ADD THIS LINE (Match your repo name exactly)
+  // THIS IS THE SECRET SAUCE:
+  base: "/portfolio/", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
