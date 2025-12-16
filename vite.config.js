189174de56c306b2e88d94ac20c428961a958585
shxrox <sharondeva8@gmail.com>
2025-12-16 21:48:57 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import path from "path"
+// import tailwindcss from "@tailwindcss/vite"
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
@@ -6,6 +22,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/portfolio/", // 👈 ADD THIS LINE (Match your repo name exactly)
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
